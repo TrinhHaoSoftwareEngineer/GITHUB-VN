200ed5e6b5782a85ceda792a7f96cfc9cf104d43
Trịnh Nguyễn Kiến Hào <hao09102007@gmail.com>
2026-07-24 22:20:12 +0700

    Second commit

--- main.cpp
+++ main.cpp
@@ -4,4 +4,9 @@ int main(){
     cout << "Hello world" << endl;
     int n; cin >> n;
     cout << n << endl; 
+    int f = 0; 
+    for(int i= 0; i < n; i++){
+        f+= i*n;
+    }
+    cout << f << endl;
 }
