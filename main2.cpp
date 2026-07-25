ef92e6825b3ac851b1e1a08bb31e539017c182f3
Trịnh Nguyễn Kiến Hào <hao09102007@gmail.com>
2026-07-25 21:04:56 +0700

    Dev branch code ver update 3

--- main2.cpp
+++ main2.cpp
@@ -1,28 +1,24 @@
 #include <iostream>
 using namespace std;
 
-<<<<<<< HEAD
-class Sound{
-=======
 class sound{
->>>>>>> dev
 public:
     virtual void makeSound() = 0;
 };
 
-<<<<<<< HEAD
-class Gun : public Sound{
+class Gun : public sound{
 public:
     void makeSound() override{
         cout << "Bang Bang Bang" << endl;
     }
 };
 
-class Boom : public Sound{
+class Boom : public sound{
 public:
     void makeSound() override{
         cout << "Boom Boom Boom" << endl;
-=======
+    }
+};
 class dog : public sound{
 public:
     void makeSound() override{
@@ -34,18 +30,14 @@ class cat : public sound{
 public:
     void makeSound() override{
         cout << "Meo meo" <<endl;
->>>>>>> dev
     }
 };
 
 int main(){
-<<<<<<< HEAD
+
     Sound* s1 = new Gun();
     Sound* s2 = new Boom();
-=======
-    sound* s1 = new dog();
-    sound* s2 = new cat();
->>>>>>> dev
+
     s1->makeSound();
     s2->makeSound();
     delete s1;
