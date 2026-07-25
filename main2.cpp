6a86a14bf2fc75fc0fad85535780af8526ffc1bb
Trịnh Nguyễn Kiến Hào <hao09102007@gmail.com>
2026-07-25 20:38:54 +0700

    Dev branch code ver update 1

--- main2.cpp
+++ main2.cpp
@@ -1,9 +1,30 @@
 #include <iostream>
 using namespace std;
 
-void bark(){
-    cout << "Gau Gau";
-}
+class sound{
+public:
+    virtual void makeSound() = 0;
+};
+
+class dog : public sound{
+public:
+    void makeSound() override{
+        cout << "Gau gau" <<endl;
+    }
+};
+
+class cat : public sound{
+public:
+    void makeSound() override{
+        cout << "Meo meo" <<endl;
+    }
+};
+
 int main(){
-    bark();
+    sound* s1 = new dog();
+    sound* s2 = new cat();
+    s1->makeSound();
+    s2->makeSound();
+    delete s1;
+    delete s2;
 }
